5dff7f597081302c56c6ee703a91ecaa5a8c846c
Muhammad Ahmed <edgemaster@Adnans-MacBook-Air.local>
2025-04-12 21:47:34 -0400

    implemented navigation stack for the screens

---
+++ fuelFinder/fuelfinder-frontend/app/(tabs)/favorites.tsx
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+export default function FavoriteScreen() {
+  return (
+    <View>
+      <Text>Plan Your Trips here!!</Text>
+    </View>
+  );
+}
---
+++ fuelFinder/fuelfinder-frontend/app/(tabs)/map.tsx
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+export default function MapScreen() {
+  return (
+    <View>
+      <Text> Map to the Nearest Station </Text>
+    </View>
+  );
+}
