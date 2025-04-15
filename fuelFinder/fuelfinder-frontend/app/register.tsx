1e36092303807043acf3e3301d78eebce500d53f
Muhammad Ahmed <edgemaster@Adnans-MacBook-Air.local>
2025-04-14 20:53:45 -0400

    added login and register screens, and styled the welcome page

--- fuelFinder/fuelfinder-frontend/app/register.tsx
+++ fuelFinder/fuelfinder-frontend/app/register.tsx
@@ -1,12 +1,14 @@
-import { View, Text, StyleSheet } from "react-native";
 import React from "react";
-export default function FavoriteScreen() {
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Register() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Plan Your Trips here!!</Text>
+    <View>
+      <Text>Please Register To Continue</Text>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: "center", alignItems: "center" },
   text: { fontSize: 18, color: "white" },
